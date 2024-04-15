81452dfb8dcd9a381dac9f54f7925466c4381fcc
mput <mputilovskij@gmail.com>
2024-04-15 18:19:57 +0500

    добавление команд /set_timer, /unset

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ async def start(update, context):
     """Отправляет сообщение когда получена команда /start"""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
-    )
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
 
 
 async def help_command(update, context):
@@ -38,14 +37,53 @@ async def date(update, context):
     await update.message.reply_text(f'Текущая дата: {date}')
 
 
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {context.job.data} cек. прошли!')
+
+
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    TIMER = int(context.args[0])
+    chat_id = update.effective_message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+
+    text = f'Вернусь через {TIMER} сек.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
+
 def main():
-    application = Application.builder().token('TOKEN').build()
+    application = Application.builder().token('7083162905:AAGDv-eTnZDWxcJ8W_86gps2Plsx25Czlm0').build()
     text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("time", time))
     application.add_handler(CommandHandler("date", date))
+    application.add_handler(CommandHandler("set_timer", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
     application.add_handler(text_handler)
 
     application.run_polling()
